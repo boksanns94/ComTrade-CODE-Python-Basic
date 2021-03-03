c186719af9828c110973553f1044b309ae4f78e6
Miloš Bokšan <bigboks1@gmail.com>
2021-03-03 20:56:49 +0100

    Vezba sa casa 5

---
+++ 5. Petlje i liste u Pythonu 3/jelkica, romb.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  3 20:27:06 2021
+
+@author: Boksan
+"""
+
+# korisnik unos n, napraviti date sablone
+
+n = int(input("Unesite n: "))
+while n < 1:
+    n = int(input("Greska. Ponovo: "))
+
+# jelkica:
+# n = 3   <- bitni su redovi, 3 reda
+#   *
+#  ***
+# *****
+
+for i in range(n):
+    print(f"{(2*i+1) * '*':^{2*n+1}}")
+
+# romb:
+# n = 3   <- bitni su redovi, 3 reda do sredine
+#   *
+#  ***
+# *****
+#  ***
+#   *
+
